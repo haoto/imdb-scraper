2b33bb8387120ea7effd06770086241e52496f1e
jimmy <jimmy@yen.me.uk>
2017-02-11 21:45:05 +0800

    * Use a real user agent string.

--- imdb/settings.py
+++ imdb/settings.py
@@ -16,8 +16,7 @@ NEWSPIDER_MODULE = 'imdb.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'imdb (+http://www.yourdomain.com)'
-USER_AGENT = 'Opera 9.6'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
